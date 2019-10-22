8ae89167bb1dea313c172233e718d81f1792cbe0
@akash.kumar <akash.kumar@ignitesol.com>
2019-10-22 18:49:31 +0530

    production ready to heroku

--- config/key_prod.js
+++ config/key_prod.js
@@ -1,4 +1,3 @@
 module.exports ={
-    mongoURL:process.env.MONGO_URL || `mongodb://akki:akki1234@ds119306.mlab.com:19306/googlesearchdata`,
-    secret:process.env.SECRET
+    mongoURL:process.env.MONGO_URL 
 }
